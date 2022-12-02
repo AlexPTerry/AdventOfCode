0ab77d59d38123fbb7ab6285af1abac0453394b7
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2022-12-02 11:25:11 +0000

    day2_1 solution

---
+++ day2_1.py
@@ -0,0 +1,11 @@
+
+input_file = open('input/input_day2_1.txt', 'r')
+
+value_dict = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+
+score = 0
+i = 0
+for line in input_file:
+    score += value_dict[line[2]] + ((value_dict[line[2]] - value_dict[line[0]] + 1) % 3) * 3
+
+print(score)
