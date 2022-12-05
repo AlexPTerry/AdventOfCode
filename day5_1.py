d8733954ee13377589801008e2b111554c0fa817
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2022-12-05 11:58:17 +0000

    day5_1 solution

--- day5_1.py
+++ day5_1.py
@@ -4,18 +4,30 @@ input_lines = open('input/input_day5_1.txt', 'r').readlines()
 
 line_break = 0
 for i in range(len(input_lines)):
-    line = input_lines[i];
+    line = input_lines[i]
 
     if line == '\n':
         line_break = i
-        num_stacks = (len(input_lines[line_break-1]) + 1) // 2
+        num_stacks = (len(input_lines[line_break-1]) + 1) // 4
         stack_list = [deque([]) for i in range(num_stacks)]
         for j in range(line_break-2, -1, -1):
             for k in range(num_stacks):
-                stack_list[k].append(input_lines[j][k*2])
+                if len(input_lines[j]) > k*4+1:
+                    block = input_lines[j][k*4+1]
+                    if block != ' ':
+                        stack_list[k].append(input_lines[j][k*4+1])
 
-    #if line_break:
-        # DO STUFF
+    elif line_break:
+        line_split = line.split()
+        i_move = int(line_split[1])
+        i_from = int(line_split[3])
+        i_to = int(line_split[5])
 
-print(stack_list)
+        for j in range(i_move):
+            stack_list[i_to-1].append(stack_list[i_from-1].pop())
+
+top_crates = ''.join([stack.pop() for stack in stack_list])
+
+print(top_crates)
+# print(stack_list)
 
