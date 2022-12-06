c9ddce491c8085912762121516e5faf12168bfd2
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2022-12-06 10:38:47 +0000

    day6_2 solution

---
+++ day6_2.py
@@ -0,0 +1,14 @@
+
+input_lines = open('input/input_day6_1.txt', 'r')
+
+char_queue = 14*['-1']
+i = 0
+for c in input_lines.read():
+    i += 1
+    char_queue = char_queue[1:] + [c]
+    char_no = [char_queue.count(char_i)-1 for char_i in char_queue]
+    if not any(char_no) and i > 14:
+        break
+
+
+print(i)
