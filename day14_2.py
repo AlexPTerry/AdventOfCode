6e00b532170b6554678788afbd138e080ea8451a
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2022-12-14 19:17:14 +0000

    day14_2 solution

--- day14_2.py
+++ day14_2.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from numpy import sign
 
 input_file = open('input/input_day14_1.txt', 'r').read().strip().split('\n')
+# input_file = open('input/input_test.txt', 'r').read().strip().split('\n')
 
 cave_data = defaultdict(lambda: '.')
 
@@ -28,9 +29,15 @@ class Cave:
         self.rightest_rock = max(self.rock_list, key=lambda z: z.real).real
         self.sand_position = 500
         self.sand_quantity = 0
+        self.max_sand_height = 500
 
     def move_sand(self):
-        if self.cave_array[self.sand_position + 1j] == '.':
+        if self.sand_position.imag + 1 == self.lowest_rock + 2:
+            self.max_sand_height = min(self.max_sand_height, self.sand_position.imag)
+            self.sand_position = 500
+            self.cave_array[self.sand_position] = 'o'
+            self.sand_quantity += 1
+        elif self.cave_array[self.sand_position + 1j] == '.':
             self.cave_array[self.sand_position] = '.'
             self.sand_position += 1j
             self.cave_array[self.sand_position] = 'o'
@@ -43,19 +50,26 @@ class Cave:
             self.sand_position += 1 + 1j
             self.cave_array[self.sand_position] = 'o'
         else:
+            self.max_sand_height = min(self.max_sand_height, self.sand_position.imag)
             self.sand_position = 500
             self.sand_quantity += 1
+            self.cave_array[self.sand_position] = 'o'
 
     def sand_loop(self):
-        while (self.sand_position.imag < self.lowest_rock and
-                self.leftest_rock <= self.sand_position.real <= self.rightest_rock):
+        while '.' in [self.cave_array[500], self.cave_array[500+1j], self.cave_array[499+1j], self.cave_array[501+1j]]:
             self.move_sand()
-        return self.sand_quantity
+        return self.sand_quantity + 1
 
 
 cave = Cave(cave_data)
 
 print(cave.sand_loop())
+
+# Cave printer!
+# for y in range(0, int(cave.lowest_rock)+2):
+#     for x in range(300, 700):
+#         print(cave.cave_array[x + y*1j], end='')
+#     print()
 
 
 
