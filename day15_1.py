f2d40768d6efcd25e6571ee33d98220763c5cf41
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2022-12-15 12:47:13 +0000

    day15_1 efficient solution

--- day15_1.py
+++ day15_1.py
@@ -6,9 +6,11 @@ def m_dist(z1, z2):
 
 
 input_file = open('input/input_day15_1.txt', 'r').read().strip().split('\n')
+yline = 2000000
 
 blocked_positions = set()
-yline = 2000000
+beacon_positions = set()
+blocked_frames = []
 for n in range(len(input_file)):
     line = input_file[n]
     line_numbers = list(map(int, re.findall(r'\d+', line)))
@@ -18,13 +20,28 @@ for n in range(len(input_file)):
     dy = int(yline - sensor_position.imag)
     abs_dy = abs(dy)
 
-    if abs_dy <= distance:
-        for i in range(-(distance-abs_dy), distance-abs_dy+1):
-            blocked_position = sensor_position+i+dy*1j
-            if blocked_position != beacon_position:
-                blocked_positions.add(blocked_position)
+    if beacon_position.imag == yline:
+        beacon_positions.add(beacon_position)
 
-print(len(blocked_positions))
+    if abs_dy <= distance:
+        new_frame = [sensor_position.real - (distance-abs_dy), sensor_position.real + (distance-abs_dy)]
+        added = True
+        while added:
+            added = False
+            for frame in blocked_frames:
+                if (new_frame[0] <= frame[0] <= new_frame[1]
+                        or new_frame[0] <= frame[1] <= new_frame[1]
+                        or frame[0] <= new_frame[0] <= frame[1]
+                        or frame[0] <= new_frame[1] <= frame[1]):
+                    blocked_frames.remove(frame)
+                    new_frame = [min(new_frame[0], frame[0]), max(new_frame[1], frame[1])]
+                    added = True
+                    break
+            if not added:
+                blocked_frames.append(new_frame)
+
+total_length = sum([frame[1] - frame[0] + 1 for frame in blocked_frames]) - len(beacon_positions)
+print(int(total_length))
 
 
 
