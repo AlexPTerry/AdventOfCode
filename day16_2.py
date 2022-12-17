4db19f61a96263f7b024bd6100a2156ff96d30a2
AlexPTerry <28978970+AlexPTerry@users.noreply.github.com>
2022-12-17 20:54:56 +0000

    day16_2 inefficient

--- day16_2.py
+++ day16_2.py
@@ -4,13 +4,8 @@ from copy import deepcopy
 
 input_file = open('input/input_day16_1.txt', 'r').read().strip().split('\n')
 start_valve = 'AA'
-max_time = 30 - 4
+max_time = 26
 time_open = 1
-
-# input_file = open('input/input_test.txt', 'r').read().strip().split('\n')
-# start_valve = 'AA'
-# max_time = 30 - 4
-# time_open = 1
 
 adjacent_valves = {}
 valve_flow_rates = {}
@@ -63,56 +58,40 @@ for valve in valuable_valves:
     valve_distances[valve] = valve_dijkstra.run_dijkstras()
 
 
-def travel(c_valve, ce_valve, r_time, re_time, c_flow, max_flow, c_valuable_valves, path, epath):
-    # print(path, r_time, c_flow)
-    true_values = {}
-    for n_valve in c_valuable_valves:
-        expected_flow = (r_time - valve_distances[c_valve][n_valve] - time_open)*valve_flow_rates[n_valve]
-        true_values[n_valve] = expected_flow
-    valve_order = sorted(true_values.keys(), key=true_values.get, reverse=True)
-    max_i = -1
-    for i in range(min(len(valve_order), 16)):
-        n_flow = c_flow
-        if r_time >= re_time:
-            n_valve = valve_order[i]
-            ne_valve = ce_valve
-            if r_time - time_open < valve_distances[c_valve][n_valve]:
-                break
-            else:
-                n_valuable_valves = deepcopy(c_valuable_valves)
-                n_valuable_valves.remove(n_valve)
-                n_flow += (r_time - valve_distances[c_valve][n_valve] - time_open) * valve_flow_rates[n_valve]
-            n_max_flow = travel(n_valve, ne_valve,
-                                        r_time - valve_distances[c_valve][n_valve] - time_open,
-                                        re_time,
-                                        n_flow, max_flow, n_valuable_valves, path + [n_valve], epath)
-        else:
-            n_valve = c_valve
-            ne_valve = valve_order[i]
-            if re_time - time_open < valve_distances[c_valve][ne_valve]:
-                break
-            else:
-                n_valuable_valves = deepcopy(c_valuable_valves)
-                n_valuable_valves.remove(ne_valve)
-                n_flow += (re_time - valve_distances[ce_valve][ne_valve] - time_open) * valve_flow_rates[ne_valve]
-            n_max_flow = travel(n_valve, ne_valve,
-                                        r_time,
-                                        re_time - valve_distances[ce_valve][ne_valve] - time_open,
-                                        n_flow, max_flow, n_valuable_valves, path, epath + [ne_valve])
-        max_flow = max(n_max_flow, max_flow)
-        if max_flow == n_max_flow:
-            max_i = i
-
-    if max_i == -1:
-        return c_flow
-    return max(c_flow, max_flow)
+location = deque([])
+path_dict = defaultdict(lambda: -1)
 
 
-# for key in valuable_valves:
-#     print(key, ':', valve_distances[key])
-#
-# print({key: valve_flow_rates[key] for key in valuable_valves})
-print(travel(start_valve, start_valve, max_time, max_time, 0, 0, valuable_valves, [start_valve], [start_valve]))
+def travel(current_valve, current_time, unvisited_valves, score):
+    end = True
+    location.append(current_valve)
+
+    valves_sorted = sorted(unvisited_valves, key=valve_distances[current_valve].get)
+
+    for next_valve in valves_sorted:
+        next_time = current_time - valve_distances[current_valve][next_valve] - time_open
+        if next_time < 0:
+            break
+        next_unvisited_valves = [valve for valve in deepcopy(unvisited_valves) if valve != next_valve]
+        next_score = score + next_time*valve_flow_rates[next_valve]
+        travel(next_valve, next_time, next_unvisited_valves, next_score)
+        end = False
+    path_dict[tuple(sorted(list(location)))] = max(score, path_dict[tuple(sorted(list(location)))])
+    location.pop()
 
 
+travel(start_valve, max_time, valuable_valves, 0)
+sorted_vals = sorted([(key, path_dict[key]) for key in path_dict.keys()], key=lambda x: x[1], reverse=True)
+
+
+def check_disjoint(t1, t2):
+    return set(t1[0][1:]).isdisjoint(set(t2[0][1:]))
+
+
+total_combinations = len(sorted_vals)
+best_two = sorted([((sorted_vals[i][0], sorted_vals[j][0]), sorted_vals[i][1] + sorted_vals[j][1])
+                   for i in range(total_combinations) for j in range(total_combinations)
+                   if check_disjoint(sorted_vals[i], sorted_vals[j])], key=lambda x: x[1], reverse=True)
+
+print(best_two[0])
 
